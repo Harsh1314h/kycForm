87d4dfd4b0286b229c7913b62fd25ea193768924
Harsh1314h <azadh2902@gmail.com>
2026-05-26 13:10:23 +0530

    Remove manual control declarations to support dynamic CodeFile compilation without duplicates

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,51 +1,6 @@
 Public Class _Default
     Inherits System.Web.UI.Page
 
-    ' =========================================================================
-    ' SECTION 1: Basic Account Information Controls
-    ' =========================================================================
-    Protected WithEvents ddlAccountType As System.Web.UI.WebControls.DropDownList
-    Protected WithEvents rdoIndividual As System.Web.UI.HtmlControls.HtmlInputRadioButton
-    Protected WithEvents rdoNonIndividual As System.Web.UI.HtmlControls.HtmlInputRadioButton
-    Protected WithEvents ddlBranch As System.Web.UI.WebControls.DropDownList
-    Protected WithEvents txtApplicationDate As System.Web.UI.WebControls.TextBox
-
-    ' =========================================================================
-    ' SECTION 2: Contact & Verification Controls
-    ' =========================================================================
-    Protected WithEvents txtEmail As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtEmailOTP As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtMobileNumber As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtAlternateMobile As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtAadhaarMobileOTP As System.Web.UI.WebControls.TextBox
-
-    ' =========================================================================
-    ' SECTION 3: Aadhaar (UIDAI) Details Controls
-    ' =========================================================================
-    Protected WithEvents txtAadhaarNumber As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtAadhaarOTP As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtAadhaarName As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtAadhaarDOB As System.Web.UI.WebControls.TextBox
-    Protected WithEvents rdoMale As System.Web.UI.HtmlControls.HtmlInputRadioButton
-    Protected WithEvents rdoFemale As System.Web.UI.HtmlControls.HtmlInputRadioButton
-    Protected WithEvents rdoOther As System.Web.UI.HtmlControls.HtmlInputRadioButton
-
-    ' =========================================================================
-    ' SECTION 5: Address Details Controls
-    ' =========================================================================
-    Protected WithEvents txtStreet As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtLocality As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtTown As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtPostOffice As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtCity As System.Web.UI.WebControls.TextBox
-    Protected WithEvents ddlState As System.Web.UI.WebControls.DropDownList
-    Protected WithEvents txtCountry As System.Web.UI.WebControls.TextBox
-    Protected WithEvents txtPincode As System.Web.UI.WebControls.TextBox
-    Protected WithEvents ddlAddressType As System.Web.UI.WebControls.DropDownList
-    Protected WithEvents rdoSameYes As System.Web.UI.HtmlControls.HtmlInputRadioButton
-    Protected WithEvents rdoSameNo As System.Web.UI.HtmlControls.HtmlInputRadioButton
-    Protected WithEvents txtPermanentAddress As System.Web.UI.WebControls.TextBox
-
     ''' <summary>
     ''' Handles the Page Load event. Pre-populates default values on initial load.
     ''' </summary>
